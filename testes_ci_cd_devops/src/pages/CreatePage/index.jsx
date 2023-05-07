1f2027d573fa2ec92fd5c365204ee0a957909b4c
Wilian Krinke <krinkewilian@gmail.com>
2023-05-06 22:03:21 -0300

    criando componente form

--- testes_ci_cd_devops/src/pages/CreatePage/index.jsx
+++ testes_ci_cd_devops/src/pages/CreatePage/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import HeaderComponent from "../../components/headerComponent/index";
 import FooterComponent from "../../components/footerComponent/index";
+import FormComponent from "../../components/formComponent/index";
 import "../styles.css";
 
 const Index = () => {
@@ -20,85 +21,7 @@ const Index = () => {
         <>
           <HeaderComponent title={"Create Page"} />
           <main className="main_general_style">
-            <section>
-              <form action="" method="post">
-                <div>
-                  <label htmlFor="name">Name:</label>
-                  <input type="text" name="name" id="name" />
-                </div>
-
-                <div>
-                  <label htmlFor="age">Age:</label>
-                  <input type="text" name="age" id="age" />
-                </div>
-
-                <div>
-                  <label htmlFor="email">E-mail:</label>
-                  <input type="text" name="email" id="email" />
-                </div>
-
-                <div>
-                  <label htmlFor="eyeColor">Eye Color:</label>
-                  <input type="text" name="eyeColor" id="eyeColor" />
-                </div>
-
-                <div>
-                  <fieldset>
-                    <legend>Genero</legend>
-                    <input
-                      type="radio"
-                      name="gender"
-                      id="gender1"
-                      value="masculino"
-                    />
-                    <label htmlFor="gender1">Masculino</label>
-
-                    <input
-                      type="radio"
-                      name="gender"
-                      id="gender2"
-                      value="feminino"
-                    />
-                    <label htmlFor="gender2">Feminino</label>
-                  </fieldset>
-                </div>
-
-                <div>
-                  <fieldset>
-                    <legend>Is Active</legend>
-                    <input
-                      type="radio"
-                      id="true"
-                      name="active"
-                      value={true}
-                      defaultChecked
-                    />
-                    <label htmlFor="true">True</label>
-
-                    <input
-                      type="radio"
-                      id="false"
-                      name="active"
-                      value={false}
-                    />
-                    <label htmlFor="false">False</label>
-                  </fieldset>
-                </div>
-
-                <div>
-                  <label htmlFor="amigos">Amigos: </label>
-                  <textarea
-                    name="amigos"
-                    id="amigos"
-                    cols="30"
-                    rows="10"
-                    maxLength='144'
-                    // resize: none css
-                    placeholder="Digite os amigos separados por ponto e virgula..."
-                  ></textarea>
-                </div>
-              </form>
-            </section>
+            <FormComponent />
           </main>
           <FooterComponent />
         </>
